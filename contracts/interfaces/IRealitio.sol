c2067871429c21ee1704635d9cfd633965fdb896
liamzebedee <liamzebedee@yahoo.com.au>
2021-02-11 14:32:14 +1000

    add interfaces

---
+++ contracts/interfaces/IRealitio.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.6.0;
+
+interface Realitio {
+    function askQuestion(uint256 template_id, string calldata question, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 nonce) external payable returns (bytes32);
+    function getContentHash(bytes32 questionId) external view returns (bytes32);
+    function getOpeningTS(bytes32 questionId) external view returns (uint32);
+    function resultFor(bytes32 questionId) external view returns (bytes32);
+}
